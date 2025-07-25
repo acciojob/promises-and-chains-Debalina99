ccfd75589957474d89928e3199bd15525da870c7
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-07-25 18:15:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,27 +3,23 @@ const form = document.getElementById('voteForm');
     const nameInput = document.getElementById('name');
     const ageInput = document.getElementById('age');
 
-    form.addEventListener('submit', function (event) {
-      event.preventDefault();
+form.addEventListener("submit",function(e){
+	e.preventDefault();
+	const name=nameInput.value.trim();
+	const age= Number(ageInput.value.trim());
 
-      const name = nameInput.value.trim();
-      const age = Number(ageInput.value.trim());
-
-      if (!name || !age) {
-        alert("Please enter valid details");
-        return;
-      }
-	const checkVotingEligibility = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (age >= 18) {
-            resolve(`Welcome, ${name}. You can vote.`);
-          } else {
-            reject(`Oh sorry ${name}. You aren't old enough.`);
-          }
-        }, 4000);
-      });
-
-      checkVotingEligibility
-        .then(message => alert(message))
-        .catch(errorMessage => alert(errorMessage));
-    });
+	if(!name || !age){
+		alert("Please enter valid details");
+		return;
+	}
+	const check=new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>=18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			}else{
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			}
+		},4000);
+	})
+	check.then(message=>alert(message)).catch(err=>alert(err));
+})
